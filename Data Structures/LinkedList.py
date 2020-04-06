25afa19d05d169a4978f2e393c367e9277f374e7
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2020-04-06 12:00:19 +0100

    Moving example to a different file.

--- Data Structures/LinkedList.py
+++ Data Structures/LinkedList.py
@@ -291,17 +291,3 @@ class LinkedList:
       if q:
         q = q.next
     r.printList()
-        
-
-myList = LinkedList()
-myList.append(5)
-myList.append(6)
-myList.append(3)
-
-myList2 = LinkedList()
-myList2.append(8)
-myList2.append(4) 
-myList2.append(2)
-myList.sum_of_two_lists(myList2)
-myList.move_tail_to_head()
-myList.printList()
