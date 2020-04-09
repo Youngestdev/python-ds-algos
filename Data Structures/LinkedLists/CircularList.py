4ea869f17b3f5cb05d2f80c335805bbdf45787fc
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2020-04-09 01:36:22 +0100

    remove_node() method and josephus circle method too. Off to bed :yawn:

--- Data Structures/LinkedLists/CircularList.py
+++ Data Structures/LinkedLists/CircularList.py
@@ -110,3 +110,36 @@ class CircularLinkedList:
     self.printList()
     print("\n")
     splitted_list.printList()
+
+  def remove_node(self, node):
+    if self.head:
+      if self.head == node:
+        cur = self.head
+        while cur.next != self.head:
+          cur = cur.next
+        if self.head == self.head.next:
+          self.head = None
+        else:
+          cur.next = self.head.next
+          self.head = self.head.next
+      else:
+        cur = self.head
+        prev = None
+        while cur.next != self.head:
+          prev = cur
+          cur = cur.next
+          if cur == node:
+            prev.next = cur.next
+            cur = cur.next
+
+  def josephus_circle(self, step):
+    cur = self.head
+
+    while len(self) > 1:
+      count = 1
+      while count != step:
+        cur = cur.next
+        count += 1
+      print("KILL :" + str(cur.data))
+      self.remove_node(cur)
+      cur = cur.next
