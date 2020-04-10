bce1662a0205b4131a49ef22a7680dd1b7fc038d
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2020-04-10 11:27:02 +0100

    Added the node class for a Double Linked List

--- Data Structures/LinkedLists/Nodes.py
+++ Data Structures/LinkedLists/Nodes.py
@@ -3,6 +3,18 @@ This consists of Node classes - Lists, Graphs ( in the future) etc
 """
 
 class Node:
+  """
+  Node class for Single and Circular list.
+  """
   def __init__(self, data):
     self.data = data
     self.next = None
+
+class DoubleNode:
+  """
+  Node class for Double List.
+  """
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+    self.prev = None
