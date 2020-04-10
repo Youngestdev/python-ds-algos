d3663d071c1e6ec0606652d1a96ef3fc61de9704
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2020-04-10 14:36:45 +0100

    Single handedly wrote a double linked list!! Madeux

--- Data Structures/LinkedLists/DoubleList.py
+++ Data Structures/LinkedLists/DoubleList.py
@@ -1,5 +1,44 @@
+from Nodes import DoubleNode
+
 class DoubleLinkedList:
+  """
+  Implementation for Double Linked List
+  """
+
   def __init__(self):
     self.head = None
 
+  def append(self, data):
+    new_node = DoubleNode(data)
+    cur = self.head
+
+    if not self.head:
+      self.head = new_node
+      return
+    
+    while cur and cur.next != None:
+      cur.prev = cur
+      cur = cur.next
+    new_node.prev = cur
+    cur.next = new_node
+    new_node.next = None
+
+  def prepend(self, data):
+    new_node = DoubleNode(data)
+    cur = self.head
+
+    if not self.head:
+      self.head = new_node
+      return
   
+    cur.prev = new_node      
+    new_node.next = cur  
+    self.head = new_node
+    
+
+  def printList(self):
+    cur = self.head
+
+    while cur:
+      print(cur.data)
+      cur = cur.next
