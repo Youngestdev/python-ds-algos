0239195c579371cc430073df34f60ae3d934d33d
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2020-04-27 00:03:42 +0100

    Little clean up

--- Data Structures/Binary Trees/BTNode.py
+++
@@ -1,6 +0,0 @@
-class Node(object):
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
--- Data Structures/LinkedLists/LinkedList.py
+++ Data Structures/LinkedLists/LinkedList.py
@@ -3,296 +3,297 @@
 
 from Nodes import Node
 
+
 class LinkedList:
-  def __init__(self):
-    self.head = None
-  
-  def append(self, data):
-    new_node = Node(data)
-    if self.head is None:
-      self.head = new_node
-      return
-    last_node = self.head
-    while last_node.next:
-      last_node = last_node.next
-    last_node.next = new_node
+    def __init__(self):
+        self.head = None
 
-  def prepend(self, data):
-    new_node = Node(data)
-    new_node.next = self.head
-    self.head = new_node
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
 
-  def insert_after_node(self, prev_node, data):
-    new_node = Node(data)
-    if not prev_node:
-      print("Previous mode doesn't exist")
-      return
-    new_node.next = prev_node.next
-    prev_node.next = new_node
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
 
-  def printList(self):
-    cur_node = self.head
-    while cur_node:
-      print(cur_node.data)
-      cur_node = cur_node.next
+    def insert_after_node(self, prev_node, data):
+        new_node = Node(data)
+        if not prev_node:
+            print("Previous mode doesn't exist")
+            return
+        new_node.next = prev_node.next
+        prev_node.next = new_node
 
-  def delete(self, key):
-    cur_node = self.head
-    if cur_node and cur_node.data == key:
-      self.head = cur_node.next
-      # set previous node to none
-      cur_node = None
-      return
+    def printList(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
 
-    Prev = None
-    while cur_node and cur_node.data != key:
-      Prev = cur_node
-      cur_node = cur_node.next
+    def delete(self, key):
+        cur_node = self.head
+        if cur_node and cur_node.data == key:
+            self.head = cur_node.next
+            # set previous node to none
+            cur_node = None
+            return
 
-      if cur_node is None:
-        return
-      
-      Prev.next = cur_node.next
-      cur_node = None
+        Prev = None
+        while cur_node and cur_node.data != key:
+            Prev = cur_node
+            cur_node = cur_node.next
 
-  def delete_by_pos(self, pos):
-    cur_node = self.head
+            if cur_node is None:
+                return
 
-    if pos == 0:
-      self.head = cur_node.next
-      cur_node = None
+            Prev.next = cur_node.next
+            cur_node = None
 
-    Prev = None
-    count = 0
-    
-    while cur_node and count != pos:
-      Prev = cur_node
-      cur_node = cur_node.next
-      count += 1
+    def delete_by_pos(self, pos):
+        cur_node = self.head
 
-      if cur_node is None:
-        return
+        if pos == 0:
+            self.head = cur_node.next
+            cur_node = None
 
-      Prev.next = cur_node.next
-      cur_node = None
+        Prev = None
+        count = 0
 
-  def len_iterative(self):
-    count = 0
-    cur_node = self.head
+        while cur_node and count != pos:
+            Prev = cur_node
+            cur_node = cur_node.next
+            count += 1
 
-    while cur_node:
-      count += 1
-      cur_node = cur_node.next
-    return count
+            if cur_node is None:
+                return
 
-  def len_recursive(self, node):
-    if node is None:
-      return 0
-    return 1 + self.len_recursive(node.next)
+            Prev.next = cur_node.next
+            cur_node = None
 
-  def node_swap(self, key_1, key_2):
-    prev_1 = None
-    curr_1 = self.head
+    def len_iterative(self):
+        count = 0
+        cur_node = self.head
 
-    while curr_1 and curr_1.data != key_1:
-      prev_1 = curr_1
-      curr_1 = curr_1.next
+        while cur_node:
+            count += 1
+            cur_node = cur_node.next
+        return count
 
-    prev_2 = None
-    curr_2 = self.head
+    def len_recursive(self, node):
+        if node is None:
+            return 0
+        return 1 + self.len_recursive(node.next)
 
-    while curr_2 and curr_2.data != key_2:
-      prev_2 = curr_2
-      curr_2 = curr_2.next
+    def node_swap(self, key_1, key_2):
+        prev_1 = None
+        curr_1 = self.head
 
-    if prev_1:
-      prev_1.next = curr_2
-    else:
-      self.head = curr_2
+        while curr_1 and curr_1.data != key_1:
+            prev_1 = curr_1
+            curr_1 = curr_1.next
 
-    if prev_2:
-      prev_2.next = curr_1
-    else:
-      self.head = curr_1
-    
-    curr_1.next, curr_2.next = curr_2.next, curr_1.next
+        prev_2 = None
+        curr_2 = self.head
 
-  def reverse(self):
-    curr_node = self.head
-    prev_node = None
-    while curr_node:
-      next_node = curr_node.next
-      curr_node.next = prev_node
-      prev_node = curr_node
-      curr_node = next_node
-    self.head = prev_node
+        while curr_2 and curr_2.data != key_2:
+            prev_2 = curr_2
+            curr_2 = curr_2.next
 
-  def merge_lists(self, list):
-    p = self.head
-    q = list.head
-    s = None
+        if prev_1:
+            prev_1.next = curr_2
+        else:
+            self.head = curr_2
 
-    if not p:
-      return q
-    if not q:
-      return p
+        if prev_2:
+            prev_2.next = curr_1
+        else:
+            self.head = curr_1
 
-    if p and q:
-      if p.data <= q.data:
-        s = p
-        p = s.next
-      else:
-        s = q
-        q = s.next
-      new_head = s
-    
-    while p and q:
-      if p.data <= q.data:
-        s.next = p
-        s = p
-        p = p.next
-      else:
-        s.next = q
-        s = q
-        q = q.next
+        curr_1.next, curr_2.next = curr_2.next, curr_1.next
 
-    if not p:
-      s.next = q
-    if not q:
-      s.next = p
+    def reverse(self):
+        curr_node = self.head
+        prev_node = None
+        while curr_node:
+            next_node = curr_node.next
+            curr_node.next = prev_node
+            prev_node = curr_node
+            curr_node = next_node
+        self.head = prev_node
 
-    return new_head
+    def merge_lists(self, list):
+        p = self.head
+        q = list.head
+        s = None
 
-  def remove_duplicates(self):
-    cur = self.head
-    prev = None
-    duplicates = dict()
+        if not p:
+            return q
+        if not q:
+            return p
 
-    while cur:
-      if cur.data in duplicates:
-        prev.next = cur.next
-        cur = None
-      else:
-        duplicates[cur.data] = 1
-        prev = cur
-      cur = prev.next
+        if p and q:
+            if p.data <= q.data:
+                s = p
+                p = s.next
+            else:
+                s = q
+                q = s.next
+            new_head = s
 
-  def nth_to_last_node(self, n):
-    total_len = self.len_iterative()
-    cur = self.head
+        while p and q:
+            if p.data <= q.data:
+                s.next = p
+                s = p
+                p = p.next
+            else:
+                s.next = q
+                s = q
+                q = q.next
 
-    while cur:
-      if total_len == n:
-        print(cur.data)
-        return cur.data
-      total_len -= 1
-      cur = cur.next
+        if not p:
+            s.next = q
+        if not q:
+            s.next = p
 
-    if cur is None:
-      return
+        return new_head
 
-  def count_occurences(self, data):
-    count = 0
-    cur = self.head
+    def remove_duplicates(self):
+        cur = self.head
+        prev = None
+        duplicates = dict()
 
-    while cur:
-      if cur.data == data:
-        count += 1
-      cur = cur.next
-    return count
+        while cur:
+            if cur.data in duplicates:
+                prev.next = cur.next
+                cur = None
+            else:
+                duplicates[cur.data] = 1
+                prev = cur
+            cur = prev.next
 
-  def rotate(self, k):
-    if self.head and self.head.next:
-      p = self.head
-      q = self.head
-      prev = None
-      count = 0
+    def nth_to_last_node(self, n):
+        total_len = self.len_iterative()
+        cur = self.head
 
-      while p and count < k:
-        prev = p
-        p = p.next
-        q = q.next
-        count += 1
-      p = prev
+        while cur:
+            if total_len == n:
+                print(cur.data)
+                return cur.data
+            total_len -= 1
+            cur = cur.next
 
-      while q:
-        prev = q
-        q = q.next
-      q = prev
+        if cur is None:
+            return
 
-      q.next = self.head
-      self.head = p.next
-      p.next = None
+    def count_occurences(self, data):
+        count = 0
+        cur = self.head
 
-  def isPalindrome_string(self):
-    s = ""
-    cur = self.head
+        while cur:
+            if cur.data == data:
+                count += 1
+            cur = cur.next
+        return count
 
-    while cur:
-      s += cur.data
-      cur = cur.next
-    return s == s[::-1]
+    def rotate(self, k):
+        if self.head and self.head.next:
+            p = self.head
+            q = self.head
+            prev = None
+            count = 0
 
-  def isPalindrome_stack(self):
-    s = []
-    cur = self.head
+            while p and count < k:
+                prev = p
+                p = p.next
+                q = q.next
+                count += 1
+            p = prev
 
-    while cur:
-      s.append(cur.data)
-      cur = cur.next
-    cur = self.head
+            while q:
+                prev = q
+                q = q.next
+            q = prev
 
-    while cur:
-      data = s.pop()
-      if cur.data != data:
-        return False
-      cur = cur.next
-    return True
-    
-  def move_tail_to_head(self):
-    if self.head and self.head.next:
-      last = self.head
-      second_to_last = None
-      while last.next:
-        second_to_last = last
-        last = last.next
-      last.next = self.head
-      second_to_last.next = None
-      self.head = last
+            q.next = self.head
+            self.head = p.next
+            p.next = None
 
-  def sum_of_two_lists(self, sList):
-    p = self.head
-    q = sList.head
-    r = LinkedList()
-    carry = 0
+    def isPalindrome_string(self):
+        s = ""
+        cur = self.head
 
-    while p or q:
-      if not p:
-        i = 0
-      else:
-        i = p.data
-      if not q:
-        j = 0
-      else:
-        j = q.data
+        while cur:
+            s += cur.data
+            cur = cur.next
+        return s == s[::-1]
 
-      s = i + j + carry
-      if s >= 10:
-        carry = 1
-        remainder = s % 10
-        r.append(remainder)
-      else:
+    def isPalindrome_stack(self):
+        s = []
+        cur = self.head
+
+        while cur:
+            s.append(cur.data)
+            cur = cur.next
+        cur = self.head
+
+        while cur:
+            data = s.pop()
+            if cur.data != data:
+                return False
+            cur = cur.next
+        return True
+
+    def move_tail_to_head(self):
+        if self.head and self.head.next:
+            last = self.head
+            second_to_last = None
+            while last.next:
+                second_to_last = last
+                last = last.next
+            last.next = self.head
+            second_to_last.next = None
+            self.head = last
+
+    def sum_of_two_lists(self, sList):
+        p = self.head
+        q = sList.head
+        r = LinkedList()
         carry = 0
-        r.append(s)
-      if p:
-        p = p.next
-      if q:
-        q = q.next
-    r.printList()
 
-  def swap_adjacent_nodes(self):
-    cur_node = self.head
+        while p or q:
+            if not p:
+                i = 0
+            else:
+                i = p.data
+            if not q:
+                j = 0
+            else:
+                j = q.data
 
-    while cur_node and cur_node.next is not None:
-      cur_node.data, cur_node.next.data = cur_node.next.data, cur_node.data
-      cur_node = cur_node.next.next
+            s = i + j + carry
+            if s >= 10:
+                carry = 1
+                remainder = s % 10
+                r.append(remainder)
+            else:
+                carry = 0
+                r.append(s)
+            if p:
+                p = p.next
+            if q:
+                q = q.next
+        r.printList()
+
+    def swap_adjacent_nodes(self):
+        cur_node = self.head
+
+        while cur_node and cur_node.next is not None:
+            cur_node.data, cur_node.next.data = cur_node.next.data, cur_node.data
+            cur_node = cur_node.next.next
