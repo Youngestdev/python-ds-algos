e6ee5922f6ff314e23af04cc0b049dccd070e077
Youngestdev <laisibizness@gmail.com>
2020-06-21 20:48:57 +0100

    Return an array of permutations

---
+++ Algorithms/Backtracking/Permutation.py
@@ -0,0 +1,34 @@
+def permutation(arr):
+    output = []
+
+    def backtrack(nums, current):
+        if len(current) == len(arr):
+            output.append(current)
+            return
+        else:
+            for i in range(len(nums)):
+                backtrack(nums[:i] + nums[i + 1:], current + [nums[i]])
+
+    backtrack(arr, [])
+    return output
+
+
+print(permutation([1, 2, 3]))
+
+
+def permuteString(sentence):
+    result = []
+
+    def backtrack(word, current):
+        if len(current) == len(sentence):
+            result.append(current)
+            return
+        else:
+            for i in range(len(word)):
+                backtrack(word[:i] + word[i + 1:], current + word[i])
+
+    backtrack(sentence, "")
+    return result
+
+
+print(permuteString("ade"))
