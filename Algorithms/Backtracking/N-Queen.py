1e418774853c9480edd2a5b43f2399c67883d959
Youngestdev <laisibizness@gmail.com>
2020-06-21 20:49:08 +0100

    Refactor

--- Algorithms/Backtracking/N-Queen.py
+++ Algorithms/Backtracking/N-Queen.py
@@ -1,18 +1,22 @@
 def SolveNQueens(n):
     arr = [0] * (n)
     count = []
+
     def placequeens(arr, r=0):
         if r == n:
             count.append(arr)
             return
         else:
-            for j in range(0, n, 1):
+            for j in range(0, n):
                 legal = True
-                for i in range(0, r, 1):
+                for i in range(0, r):
                     if (arr[i] == j) or (arr[i] == j + r - i) or (arr[i] == j - r + i):
                         legal = False
                 if legal:
                     arr[r] = j
-                    placequeens(arr, r+1)
+                    placequeens(arr, r + 1)
+
     placequeens(arr, 0)
     return len(count)
+
+print(SolveNQueens(4))
