88d362efe7c08b7d99ff9e02dd8bccbfc612231a
Youngestdev <laisibizness@gmail.com>
2020-06-22 01:11:34 +0100

    Fibonacci sturv

--- Algorithms/Dynamic Programming/Fibonacci.py
+++ Algorithms/Dynamic Programming/Fibonacci.py
@@ -3,12 +3,32 @@ def fastRecursiveFibonacci(n):
         return 0, 1
     m = n // 2
     _pvr, _cur = fastRecursiveFibonacci(m)
-    prev = _pvr**2 + _cur**2
+    prev = _pvr ** 2 + _cur ** 2
     curr = _cur * ((2 * _pvr) + _cur)
     nxt = prev + curr
     if n % 2 == 0:
         return prev, curr
     else:
         return curr, nxt
-print(fastRecursiveFibonacci(9))
-print(fastRecursiveFibonacci(10))
+
+
+def fibonacci(n):
+    Fibonacci = [0, 1] + [0] * (n - 2)
+    prev, curr = 1, 0
+    for i in range(2, n):
+        Fibonacci[i] = Fibonacci[i - 1] + Fibonacci[i - 2]
+    return Fibonacci[n - 1]
+
+
+def iterativeFibonacci(n):
+    prev, curr = 1, 0
+    for i in range(1, n):
+        _next = prev + curr
+        prev = curr
+        curr = _next
+    return curr
+
+
+# print(fibonacci(9))
+print(fastRecursiveFibonacci(5000))
+# print(iterativeFibonacci(9))
