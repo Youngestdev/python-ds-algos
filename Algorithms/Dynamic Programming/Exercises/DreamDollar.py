760d4e9f5c9827770a56da0b5b3ccbf030720e20
Youngestdev <laisibizness@gmail.com>
2020-06-24 00:25:14 +0100

    Dream dollar & Longest increasing subsequence

--- Algorithms/Dynamic Programming/Exercises/DreamDollar.py
+++ Algorithms/Dynamic Programming/Exercises/DreamDollar.py
@@ -21,6 +21,16 @@ def minNoOfBills(change):
     backtrack(arr, [])
     return res or -1
 
+def minNoOfBills2(amount):
+    dp = [float('inf')] * (amount + 1)
+    dp[0] = 0
+    denominations = [1, 4, 13, 28, 52, 91, 365]
 
-print(minNoOfBills(240))
-print(minNoOfBills(20))
+    for currency in denominations:
+        for i in range(currency, amount+1):
+            dp[i] = min(dp[i], dp[i-currency] + 1)
+
+    print(dp)
+    return dp[amount] if dp[amount] != float('inf') else -1
+
+print(minNoOfBills2(121))
--- Algorithms/Dynamic Programming/LongestIncreasingSubsequence.py
+++ Algorithms/Dynamic Programming/LongestIncreasingSubsequence.py
@@ -1,57 +1,19 @@
 def fastLIS(arr):
-    if arr == sorted(arr):
-        return len(arr)
-
-    n = len(arr) - 1
     arr.insert(0, float('-inf'))
-
-    def LIFirst(v):
-        _best = 0
-        for k in range(v + 1, n):
-            if arr[k] > arr[v]:
-                _best = max(_best, LIFirst(k))
-        return 1 + _best
-
-    for i in range(n, -1, -1):
-        LIFirst(i)
-        for j in range(i + 1, n):
-            if arr[j] > arr[i] and (1 + LIFirst(j) > LIFirst(i)):
-                LIFirst(i) + LIFirst(j)
-    return LIFirst(0)
-
-
-def longestIncreasingSubsequence(arr):
-    result = [1] + [0] * (len(arr) - 1)
-
-    for i in range(1, len(arr)):
-        if arr[i - 1] < arr[i]:
-            result[i] = result[i - 1] + 1
-        else:
-            result[i] = 1
-    print(arr, result)
-    return max(result)
-
-
-def longestIncreasingSubsequence2(arr):
-    result = [1] + [0] * (len(arr) - 1)
-    # maxSum = 1
     n = len(arr)
-    for i in range(1, n):
-        maxSum = 1
-        for j in range(1, i):
-            if arr[j] < arr[i] and result[j] + 1 > maxSum:
-                maxSum = result[j] + 1
-        result[i] = maxSum
-    maxSum = 0
+    dp = [[0 for i in range(n+1)] for j in range(n+1)]
+    for i in range(n):
+        dp[i][n] = 0
 
-    for k in range(n):
-        if result[k] > maxSum:
-            maxSum = result[k]
-    print(result)
-    return maxSum
+    for j in range(n-1, 0, -1):
+        for k in range(0, j):
+            keep = 1 + dp[j][j+1]
+            # print("Value of Keep: ", keep)
+            skip = dp[k][j+1]
+            if arr[k] >= arr[j]:
+                dp[k][j] = skip
+            else:
+                dp[k][j] = max(keep, skip)
+    return dp[0][1]
 
-
-print(longestIncreasingSubsequence2([1, -1, 2, 3, 8, 9, 0, 12, 13, 14, 15]))
-print(longestIncreasingSubsequence([-2, -1]))
-print(longestIncreasingSubsequence([10, 9, 2, 5, 3, 7, 101, 18]))
-print(longestIncreasingSubsequence([3, 2, 1]))
+print(fastLIS([10,9,2,5,3,7,101,18]))
